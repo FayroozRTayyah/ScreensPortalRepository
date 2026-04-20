3cd83bc4b3baad68c8971b876ad372775824aed3
arsaqqa <arsaqqa@nablus.org>
2026-04-20 09:13:35 +0200

    database

---
+++ Screens/Screens/Migrations/20260420071252_second.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Screens.Migrations
+{
+    /// <inheritdoc />
+    public partial class second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "imagetoDate",
+                table: "images",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "imagefromDate",
+                table: "images",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AddColumn<int>(
+                name: "imageOrder",
+                table: "images",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "imageScreenId",
+                table: "images",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "imageOrder",
+                table: "images");
+
+            migrationBuilder.DropColumn(
+                name: "imageScreenId",
+                table: "images");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "imagetoDate",
+                table: "images",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "imagefromDate",
+                table: "images",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
