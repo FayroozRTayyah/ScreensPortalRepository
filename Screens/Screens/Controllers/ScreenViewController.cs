c3eaad253621740fb6a4cae667d831c563ee5131
frtayyah <municipaltynablus@gmail.com>
2026-04-23 10:43:08 +0200

    AutoplayTimeout /appsettings

--- Screens/Screens/Controllers/ScreenViewController.cs
+++ Screens/Screens/Controllers/ScreenViewController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Screens.data;
 using Screens.Models;
 using System.Diagnostics;
@@ -15,15 +16,25 @@ namespace Screens.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly AppDbContext _context;
-        public ScreenViewController(ILogger<HomeController> logger, AppDbContext context)
+        private readonly IConfiguration _configuration;
+
+        public ScreenViewController(ILogger<HomeController> logger, AppDbContext context, IConfiguration configuration)
         {
             _logger = logger;
             _context = context;
+            _configuration = configuration;
+
         }
         [Route("ScreenView/{screenID?}")]
         public IActionResult Index(int screenID )
-        {                       
-                var images = _context.images
+        {
+
+            var timeout = _configuration.GetValue<int>("CarouselSettings:AutoplayTimeout");
+
+            ViewBag.AutoplayTimeout = timeout;
+
+
+            var images = _context.images
                                .Where(x => x.image_status == 1  && (x.imageScreenId == screenID || x.imageScreenId == global.allScreens) && (DateTime.UtcNow >=x.imagefromDate) && (DateTime.UtcNow<=x.imagetoDate ))
                               .OrderBy(x => x.imageOrder)
                                .ToList();
